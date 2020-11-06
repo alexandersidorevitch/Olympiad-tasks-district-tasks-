96a8a83dbe8199964289d675dbbbc8ba7640e37b
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-11-06 17:28:18 +0300

    Домашка 05.11

---
+++ Первое занятие (05.11)/Домашка(05.11)/236A/236A/236A.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+int countOfDifferentLetters(string s) {
+	//SET создает коллекцию данных, из не повторяющихся элементов
+	set<char> differentLetters;
+	for (auto letter : s) {
+		differentLetters.insert(letter);
+	}
+	return differentLetters.size();
+}
+
+bool isOdd(int n) {
+	return n % 2;
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	
+	if (isOdd(countOfDifferentLetters(s)))
+	{
+		cout << "IGNORE HIM!";
+	}
+	else {
+		cout << "CHAT WITH HER!";
+	}
+}
