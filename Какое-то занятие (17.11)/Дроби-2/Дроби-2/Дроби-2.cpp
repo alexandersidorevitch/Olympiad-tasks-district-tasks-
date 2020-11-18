02293527d0487900194791df69da8c1a0f056a95
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-11-18 23:47:50 +0300

    Дроби 2

---
+++ Какое-то занятие (17.11)/Дроби-2/Дроби-2/Дроби-2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <set>
+#define ll long long
+using namespace std;
+ll NOD(ll a, ll b) {
+	a = abs(a);
+	b = abs(b);
+	while (a && b)
+		if (a > b) a %= b;
+		else b %= a;
+	return a + b;
+}
+int main()
+{
+	ll n, count = 0;
+
+	ll firstNumber, secondNumber, nod;
+	ifstream input("input.txt");
+	input >> n;
+	set <pair<ll, ll>> fractions;
+	for (size_t i = 0; i < n; ++i)
+	{
+		//cin >> firstNumber >> secondNumber;
+		input >> firstNumber >> secondNumber;
+		nod = NOD(firstNumber, secondNumber);
+		fractions.insert(make_pair(firstNumber / nod, secondNumber / nod));
+	}
+	input.close();
+
+	ofstream file("output.txt");
+	file << fractions.size();
+	file.close();
+
+}
