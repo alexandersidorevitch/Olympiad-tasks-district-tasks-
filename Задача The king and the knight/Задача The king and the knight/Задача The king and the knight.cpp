a640f388c030b4787a9b5f9b7a87e5e1867e0eae
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-11-24 22:45:31 +0300

    Еще одно обновление

--- Задача The king and the knight/Задача The king and the knight/Задача The king and the knight.cpp
+++ Задача The king and the knight/Задача The king and the knight/Задача The king and the knight.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-char moveKnight(pii posKing, pii posKhight, vector<pii> movesKnight) {
-	if ((abs(posKhight.first - posKing.first) == 1 && abs(posKhight.second  - posKing.second) == 2) || (abs(posKhight.first - posKing.first) == 2 && abs(posKhight.second - posKing.second) == 1))
+char moveKnight(pii posKing, pii posKhight) {
+	if (posKhight.first >= 0 && posKhight.first <= 7 && posKhight.second >= 0 && posKhight.second <= 7 && ((abs(posKhight.first - posKing.first) == 1 && abs(posKhight.second  - posKing.second) == 2) || (abs(posKhight.first - posKing.first) == 2 && abs(posKhight.second - posKing.second) == 1)))
 	{
 		return 'Y';
 	}
@@ -30,15 +30,11 @@ char moveKnightDouble(pii posKing, pii posKhight, vector<pii> movesKnight) {
 	int newPosKhightY;
 	for (auto moveFirst : movesKnight)
 	{
-		for (auto moveSecond : movesKnight)
+		
+		if (moveKnight(posKing, make_pair(moveFirst.first + posKhight.first, moveFirst.second + posKhight.second)) == 'Y')
 		{
-			newPosKhightX = posKhight.first + moveFirst.first;
-			newPosKhightY = posKhight.second + moveFirst.second;
-			if ((newPosKhightX >= 0 && newPosKhightX <= 7) && newPosKhightX + moveSecond.first == posKing.first && (newPosKhightY >= 0 && newPosKhightY <= 7) && newPosKhightY + moveSecond.second == posKing.second)
-			{
-				return 'Y';
-			}
-		}
+			return 'Y';
+		} 
 	}
 	return 'N';
 }
@@ -73,7 +69,7 @@ int main()
 	knightPos = make_pair(s[0] - 'a', s[1] - '1');
 	cin.close();
 
-	char anwerKnight = moveKnight(kingPos, knightPos, movesKnight);
+	char anwerKnight = moveKnight(kingPos, knightPos);
 	if (anwerKnight == 'Y')
 	{
 		cout << anwerKnight << 'N';
