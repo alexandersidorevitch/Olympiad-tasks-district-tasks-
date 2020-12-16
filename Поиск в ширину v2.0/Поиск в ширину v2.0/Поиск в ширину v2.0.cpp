9b1e240f90f997b942b1d0fd10bb8f981883ec51
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-12-16 12:15:26 +0300

    Графы. Занятие 1

---
+++ Поиск в ширину v2.0/Поиск в ширину v2.0/Поиск в ширину v2.0.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+using namespace std;
+struct Point
+{
+	string name;
+	string inheritor;
+	bool isVisited;
+	bool operator ==(string other) {
+		if (name == other)
+		{
+			return true;
+		}
+		return false;
+	}
+};
+string printPath(string startCity, string endCity, vector<Point> Cities) {
+	Point ptr = *find(Cities.begin(), Cities.end(), endCity);
+	string answer = "";
+	while (ptr.name != startCity)
+	{
+		answer = " -> " + ptr.name + answer;
+		ptr = *find(Cities.begin(), Cities.end(), ptr.inheritor);
+	}
+	return startCity + answer;
+}
+int main()
+{
+	map<string, vector<string>> Graph;
+	int countOfCities, countOfRoads;
+	string firstCity, secondCity;
+	Point city;
+	string cityName;
+	vector<Point> allCities;
+
+	cin >> countOfCities;
+	for (size_t i = 0; i < countOfCities; i++)
+	{
+		cin >> cityName;
+		city.name = cityName;
+		city.inheritor = "";
+		city.isVisited = false;
+		allCities.push_back(city);
+	}
+
+	cin >> countOfRoads;
+	for (size_t i = 0; i < countOfRoads; i++)
+	{
+		cin >> firstCity >> secondCity;
+		Graph[firstCity].push_back(secondCity);
+		Graph[secondCity].push_back(firstCity);
+	}
+
+	string startCity, endCity, currentCity;
+	cin >> startCity >> endCity;
+
+
+
+	(*find(allCities.begin(), allCities.end(), startCity)).isVisited = true;
+
+	queue<string> Queue;
+	Queue.push(startCity);
+	while (!Queue.empty())
+	{
+		currentCity = Queue.front();
+		Queue.pop();
+		for (auto town: Graph[currentCity])
+		{
+			auto el = find(allCities.begin(), allCities.end(), town);
+			if (not (*el).isVisited)
+			{
+				(*el).isVisited = true;
+				Queue.push((*el).name);
+				(*el).inheritor = currentCity;
+ 			}
+
+		}
+
+	}
+	if ((*find(allCities.begin(), allCities.end(), endCity)).isVisited)
+	{
+		cout << printPath(startCity, endCity, allCities);
+	}
+	else
+	{
+		cout << "Пути нет";
+	}
+}
---
+++ Районная олимпиада/ОЛИМПИАДА Задача B/ОЛИМПИАДА Задача B/ОЛИМПИАДА Задача B.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <set>
+#include <string>
+
+using namespace std;
+struct Interval {
+	int a;
+	int b;
+	bool operator<(const Interval& other) const {
+
+		if (a < other.a)
+		{
+			return true;
+		}
+		if (a > other.a)
+			return false;
+		if (b < other.b)
+		{
+			return false;
+		}
+		return true;
+	}
+};
+int main()
+{
+	int n, a, b;
+	set<Interval> intervals;
+	Interval tmpInteval;
+	string s;
+	ios::sync_with_stdio(false);
+
+	getline(cin, s);
+	n = stoi(s);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		getline(cin, s, ' ');
+		a = stoi(s);
+		getline(cin, s);
+		b = stoi(s);
+		tmpInteval.a = a;
+		tmpInteval.b = b;
+		intervals.insert(tmpInteval);
+
+	}
+	int count = intervals.size();
+	set<Interval>::iterator first_it = intervals.begin(), second_it = intervals.begin(), tmp_it;
+
+	for (first_it; first_it != intervals.end(); first_it = second_it)
+	{
+		tmp_it = first_it;
+		for (second_it++; second_it != intervals.end() && (*first_it).a <= (*second_it).a && (*first_it).b >= (*second_it).b; ++second_it)
+		{
+			--count;
+		}
+	}
+	cout << count;
+}
